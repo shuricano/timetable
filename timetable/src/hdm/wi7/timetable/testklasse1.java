382b2fc1ef6063b5b84704b973c8f1240734a2ce
shuricano <fabianroeber@gmail.com>
2013-11-14 12:40:41 +0100

    initial commit

---
+++ timetable/src/hdm/wi7/timetable/testklasse1.java
@@ -0,0 +1,12 @@
+package hdm.wi7.timetable;
+
+public class testklasse1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("ALLAHUAKBAR");
+	}
+
+}
