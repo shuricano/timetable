ce2a0ed5fc18d55516ae0fd53fe29a39a3d5c0e3
shuricano <fabianroeber@gmail.com>
2013-12-05 15:40:24 +0100

    equals methode

--- timetable/src/hdm/wi7/timetable/classes/TimetableObject.java
+++ timetable/src/hdm/wi7/timetable/classes/TimetableObject.java
@@ -3,9 +3,11 @@ package hdm.wi7.timetable.classes;
 import java.io.Serializable;
 
 public class TimetableObject implements Serializable {
-
+	
+	//Version des Serializable Interfaces	
 	private static final long serialVersionUID = 1L;
 	
+	//Id wird an jedes Stundenplan Objekt weitergegeben	
 	private int id = 0;
 	
 	public int getId() {
@@ -15,6 +17,21 @@ public class TimetableObject implements Serializable {
 	public void setId(int id){
 		this.id = id;
 	}
+	
+	public int hashcode() {
+		return this.id;
+	}
+	
+	
+	//@author thies
+	public boolean equals(Object o) {
+	if (o != null && o instanceof TimetableObject){
+		try ()
+	}
+	
+	}
+	
+	
 }
 
 
